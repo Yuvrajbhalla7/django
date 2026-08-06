a9b1a828b38dcc7b610c18ee961a36923dd9a662
yuvra bhalla <yuvrajbhalla44@gmail.com>
2026-08-06 13:21:29 +0530

    updates

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    email = models.EmailField(null=True)
+    image = models.ImageField(null=True)
+    file = models.FileField(null=True)
+
+
+
+class car(models.Model):
+    car_name = models.CharField( max_length=50)
+    speed = models.IntegerField(default=50)
